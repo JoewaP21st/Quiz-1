108890b30b691a8739f7a04b36b681793dd65498
Zuhua Peng <pengzuhua2014@gmail.com>
2021-09-29 00:52:25 -0400

    part 2 before top label linked

--- Quiz 1/ViewController.swift
+++ Quiz 1/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        messageLabel.text="Hello there"
+        
+    }
+    
 }
 
